c90590bf76e94c990e3ca0c968a40fc9b8715333
avicity7 <74177090+avicity7@users.noreply.github.com>
2022-06-14 20:35:15 +0800

    Remove Statusbar

--- App.js
+++ App.js
@@ -5,17 +5,32 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "./screens/Login";
 import Chats from "./screens/Chats";
+import {
+  useFonts,
+  Sora_400Regular,
+  Sora_600SemiBold,
+  Sora_700Bold,
+} from "@expo-google-fonts/sora";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Sora_400Regular,
+    Sora_600SemiBold,
+    Sora_700Bold,
+  });
+
   return (
+    <>
+    <StatusBar hidden />
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen options={{headerShown: false}} name="Login" component={Login} />
         <Stack.Screen name="Chats" component={Chats} />
       </Stack.Navigator>
     </NavigationContainer>
+    </>
   );
 }
 
--- screens/Login.js
+++ screens/Login.js
@@ -1,14 +1,25 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { KeyboardAvoidingView } from "react-native";
 
 const Login = () => {
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <KeyboardAvoidingView style={styles.container}>
+      <Text style = {styles.normalText}>LoginScreen</Text>
+    </KeyboardAvoidingView>
   );
 };
 
 export default Login;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: { 
+    flex: 1,
+    backgroundColor: "#333",
+  },
+
+  normalText: { 
+    color : "#fff",
+    fontFamily : "Sora_400Regular",
+  }
+});
